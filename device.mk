fdbe8e684430230c4070089b5a8c5958b22d6e95
Dyrex2004 <dyrex2004@gmail.com>
2020-10-01 19:06:15 +0200

    [SQUASH] cepheus: Define Blur the better way
    Squash of 3 commits
    
    This reverts commit 0792b7e936ae269ef334407eec00b74e2c8f6db7.
    
    cepheus: Enable blurs, under dev-option
    
    Test: make
    Test: manual
    Bug: 149792636
    Change-Id: I3352b34739f5f45efa4088044de0e030449ff866
    
    cepheus: Default blurs to on on coral and flame
    
    Bug: 149792636
    Test: make
    Change-Id: Id78b72bd3925db21c4c963298c3684e3b54ba43a

--- device.mk
+++ device.mk
@@ -152,6 +152,11 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3-service.clearkey \
     android.hardware.drm@1.3-service.widevine
 
+# Enable blurs
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.surface_flinger.supports_background_blur=1 \
+    ro.sf.blurs_are_expensive=1
+
 # Fingerprint
 PRODUCT_PACKAGES += \
     vendor.lineage.biometrics.fingerprint.inscreen@1.0-service.cepheus
